a415f241ae086c828d3ee42584f7a5b2de9e7231
Chris Aholt <35589544+chrisaholt@users.noreply.github.com>
2024-08-27 21:50:38 -0700

    Adds text input for side lengths

--- create_box.py
+++ create_box.py
@@ -2,6 +2,16 @@
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
 
+def get_input(ui, text_prompt, title, default_value):
+    value, cancelled = ui.inputBox(text_prompt, title, default_value)
+    if cancelled:
+        exit(1)
+    if not value:
+        raise ValueError("Unknown value entered.")
+    print(value)
+    print(type(value))
+    return value
+
 def run(context):
     ui = None
     try:
@@ -11,6 +21,21 @@ def run(context):
         design = app.activeProduct
         root_component = design.rootComponent
 
+        # Set up units.
+        units_manager = design.unitsManager
+        units_name = units_manager.defaultLengthUnits
+
+        # Create a new value input box for Length, Width, and Height
+        inputs = adsk.core.CommandInputs.cast(None)
+
+        length = get_input(ui, f"Enter the length of the box ({units_name}):", "Box Length", "100")
+        width = get_input(ui, f"Enter the width of the box ({units_name}):", "Box Width", "50")
+        height = get_input(ui, f"Enter the height of the box ({units_name}):", "Box Height", "25")
+
+        length = units_manager.evaluateExpression(length, units_name)
+        width = units_manager.evaluateExpression(width, units_name)
+        height = units_manager.evaluateExpression(height, units_name)
+
         # Create a new sketch on the xy plane
         sketches = root_component.sketches
         xy_plane = root_component.xYConstructionPlane
@@ -19,7 +44,7 @@ def run(context):
         # Draw a rectangle for the base of the box
         sketch.sketchCurves.sketchLines.addTwoPointRectangle(
             adsk.core.Point3D.create(0, 0, 0),
-            adsk.core.Point3D.create(100, 100, 0))
+            adsk.core.Point3D.create(length, width, 0))
         
         # Extrude the rectangle to create the box
         square_profile = sketch.profiles.item(0)
@@ -27,7 +52,7 @@ def run(context):
         extrude_input = extrudes.createInput(
             square_profile, adsk.fusion.FeatureOperations.NewBodyFeatureOperation
         )
-        distance = adsk.core.ValueInput.createByReal(50)
+        distance = adsk.core.ValueInput.createByReal(height)
         extrude_input.setDistanceExtent(False, distance)
         extrudes.add(extrude_input)
         
